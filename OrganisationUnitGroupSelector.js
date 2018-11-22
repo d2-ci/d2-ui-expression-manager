ee7830d391eac7c68707a197b21e565b111101b1
joakimia <joakimamundsen91@gmail.com>
2018-11-22 14:54:23 +0000

    cf21c34 v5.0.6

---
+++ OrganisationUnitGroupSelector.js
@@ -0,0 +1,25 @@
+import { Observable } from 'rxjs';
+import { isFunction } from 'lodash';
+
+import { ListSelectWithLocalSearch } from '@dhis2/d2-ui-core';
+import { withPropsFromObservable } from '@dhis2/d2-ui-core';
+
+import { getAllObjectsWithFields } from './data-helpers';
+
+var organisationUnitGroupSelectorProps$ = Observable.fromPromise(getAllObjectsWithFields('organisationUnitGroup')).map(function (organisationUnitGroups) {
+    return {
+        source: organisationUnitGroups.map(function (model) {
+            return { value: model.id, label: model.displayName };
+        }),
+        onItemDoubleClick: function onItemDoubleClick(value) {
+            var ougFormula = ['OUG{', value, '}'].join('');
+
+            // `this` is the react component props object
+            if (isFunction(this.onSelect)) {
+                this.onSelect(ougFormula);
+            }
+        }
+    };
+});
+
+export default withPropsFromObservable(organisationUnitGroupSelectorProps$, ListSelectWithLocalSearch);
